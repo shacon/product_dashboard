59bc3cf0067051c499b71ab7bc502a893391c2b0
shacon <shannonjc143@gmail.com>
2025-06-10 15:11:28 -0500

    Add endpoint, add pycache to gitignore

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 
-
 Running Locally
-
 
 1. Install uv (if you don't already have it)
 2. git clone
@@ -9,13 +7,11 @@ Running Locally
 4. Install dependencies and create virutal env with `uv sync`
 5. Activate the virtual environment with `source .venv/bin/activate` (optional - can otherwise run commands with uv which will run within virtualenv)
 
-
 Make Migrations
 `uv run python manage.py makemigrations`
 
 Apply Migrations
 `uv run python manage.py migrate`
-
 
 Running Tests
 `uv run pytest`
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the dashboard index.")
+
+
+def products(request):
+  queryset = Product.objects.all()
+  if request.GET.get('best_rated') is not None:
+    queryset = queryset.filter(rating__isnull=False).order_by('-rating')
+  elif request.GET.get('most_reviewed') is not None:
+    queryset = queryset.filter(total_reviews__isnull=False).order_by('-total_reviews')
+  limit = request.GET.get('limit')
+  if limit:
+    queryset = queryset[:int(limit)]
+
+  products_list = []
+  for product in queryset:
+      products_list.append({
+          'id': product.id,
+          'name': product.name,
+          'price': str(product.price),
+          'total_reviews': product.total_reviews,
+          'rating': str(product.rating) if product.rating else None,
+          'image_url': product.image_url,
+      })
+    
+  return JsonResponse({'products': products_list})
