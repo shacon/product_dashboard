40be5d239df8807bfb9153ca2e20e731a1e9b407
shacon <shannonjc143@gmail.com>
2025-06-12 12:41:40 -0500

    product gallery component

--- product_frontend/src/ProductGallery/ProductGallery.tsx
+++ product_frontend/src/ProductGallery/ProductGallery.tsx
@@ -9,7 +9,7 @@ interface ProductGalleryProps {
 }
 
 function ProductGallery({ products }: ProductGalleryProps) {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 5;
   const totalPages = Math.ceil(products.length / itemsPerPage);
 
@@ -23,16 +23,41 @@ function ProductGallery({ products }: ProductGalleryProps) {
   if (!products) {
     return <div>Loading product...</div>;
   }
+
+  const leftClick = () => {
+    console.log("left Clicked!");
+    setCurrentPage(currentPage - 1);
+  };
+
+  const rightClick = () => {
+    console.log("right Clicked!");
+    console.log(currentPage >= totalPages);
+    console.log("total pages", totalPages);
+    console.log("current pages", currentPage);
+    setCurrentPage(currentPage + 1);
+  };
+
+  const disabled = (direction: "left" | "right"): boolean => {
+    if (direction === "left") {
+      return currentPage <= 0;
+    }
+    if (direction === "right") {
+      return currentPage >= totalPages - 1;
+    }
+    return false;
+  };
+
   return (
     <div className={styles.gallery}>
-      <button>
+      <button onClick={leftClick} disabled={disabled("left")}>
         <ChevronLeft size={50} />
       </button>
-
-      {currentProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-      <button>
+      <div className={styles.productWrapper}>
+        {currentProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <button onClick={rightClick} disabled={disabled("right")}>
         <ChevronRight size={50} />
       </button>
     </div>
